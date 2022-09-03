7ed4eb30a29f251ced717d01eac45528511861d9
mpusinhol <matheus@kramphub.com>
2022-09-03 12:26:18 +0200

    Add open api

--- src/main/java/com/mpusinhol/temperaturesensorapi/resource/TemperatureResource.java
+++ src/main/java/com/mpusinhol/temperaturesensorapi/resource/TemperatureResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -17,7 +18,7 @@ import java.net.URI;
 import static com.mpusinhol.temperaturesensorapi.mapper.TemperatureMapper.toDTO;
 import static com.mpusinhol.temperaturesensorapi.mapper.TemperatureMapper.toModel;
 
-@Controller
+@RestController
 @RequestMapping(value = "/temperatures")
 @RequiredArgsConstructor
 public class TemperatureResource {
